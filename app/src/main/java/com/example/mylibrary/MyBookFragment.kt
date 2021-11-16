138440b3921664cb7441a3aecd99090a0abdd6b3
MartAna <92936649+MartAna@users.noreply.github.com>
2021-11-16 22:10:08 +0300

    main fragment

--- app/src/main/java/com/example/mylibrary/MyBookFragment.kt
+++ app/src/main/java/com/example/mylibrary/MyBookFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MyBookFragment : Fragment() {
+
+    private lateinit var floatingActionButton: FloatingActionButton
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        floatingActionButton = view.findViewById(R.id.floating_action_button)
+
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_book, container, false)
     }
